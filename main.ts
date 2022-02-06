2d033fdcc380d3b1704256511bd57235250a96bb
Hananoshika Yomaru <brenkysbwim@gmail.com>
2022-02-06 23:57:50 +0800

    init

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, ToggleComponent } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	showDryRunCommands : boolean 
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	showDryRunCommands : false 
 }
 
 export default class MyPlugin extends Plugin {
@@ -15,67 +15,7 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		addCommands(this)
 	}
 
 	onunload() {
@@ -91,21 +31,45 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+function addCommands (plugin : Plugin ) { 
+	const prefix = "cluster-command"
+	plugin.addCommand ({
+		id : `${prefix}-propagate-this` , 
+		name : "propagate this note", 
+		callback : () => { 
+			throw new Error("not implmeneted ")
+		}
+	}) 
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	plugin.addCommand ( { 
+		id : `${prefix}-propagate-all` , 
+		name : "propagate all notes" , 
+		callback : () => {
+			throw new Error("not implmeneted ")
+		}
+	}) 
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+	plugin.addCommand ( { 
+		id : `${prefix}-isolate-this` , 
+		name : "isolate this cluster" , 
+		callback : () => { 
+			throw new Error ("not implemented") 
+		}
+	})
+
+	plugin.addCommand ( { 
+		id : `${prefix}-merge-clusters` , 
+		name  : "merge some clusters" , 
+		callback : () => { 
+			throw new Error("not implmeneted ")
+		}
+	})
+
+	plugin.addCommand( { 
+		id : `${prefix}-`
+	})
+} 
+
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
@@ -116,22 +80,14 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		new Setting(this.containerEl)
+			.setName('Show Dry Run Commands')
+			.setDesc('Dry run commands run your commands, showing the expected result but not actually changing the content.')
+			.addToggle((toggle) => {
+				toggle.onChange(async value => { 
+					this.plugin .settings.showDryRunCommands  = value 
+					await this.plugin.saveSettings ();
+				})
+			})
 	}
 }
